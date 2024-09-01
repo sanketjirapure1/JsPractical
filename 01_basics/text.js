b0930692208729777410ca9db79b7344de820117
sanketjirapure1 <76209110+sanketjirapure1@users.noreply.github.com>
2024-09-01 09:30:47 +0000

    new changes

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,17 @@
-console.log("Sanket");  
+const accountNo = 1234567;
+let rollNo = 123;
+var accountPassword ="1234567";
+accountCity = "pune";
+// console.log(accountCity);
+// accountNo =123;  
+ rollNo = "2233333333"
+// console.table([accountNo,accountCity,rollNo]);
+ let i=[];
+ for( let i=1;i<=5;i++){
+    // return i    
+    // bbhh
+    console.log(i);
+}
+ let arr = [1,2,3,4,5];
+ let arrs = arr.map((num) => num);
+ console.log(arrs);
